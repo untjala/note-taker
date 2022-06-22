7f3c2c3c5b235a64ea634bdc8a3aa17943ec3a50
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-22 15:35:47 -0400

    wrote get routes

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 //Pulling in required dependencies 
 const express = require('express');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const { application } = require('express');
+//Sets the port--env for heroku, 3001 for fallback
 const PORT = process.env.PORT || 3001;
 //Sets up express.js as an app
 const app = express();
-//Public static page assests
-app.use(express.static('public'));
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 //Tells express to format into JSON
 app.use(express.json());
+//Public static page assests
+app.use(express.static('public'));
 
+//API routes
+//Will return index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 )
+//Will return notes.html
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname),'/public/notes.html')
 )
+//Gets notes, reads from the database and parses through JSON
+app.get('/api/notes', (req,res) => {
+  fs.readFile(path.join(__dirname,'./db/notes.json'), 'utf-8', (error,data) => {
+    if (error) throw error;
+    res.json(JSON.parse(data));
+  });
+});
+
+
+
+
+
+
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
