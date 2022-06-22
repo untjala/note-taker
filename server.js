c17986dbca2da78e295c70ad49951218bc077aea
Jala Unterreiner <jala.binbek@outlook.com>
2022-06-22 13:12:46 -0400

    set git request for notes to return notes.html

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const app = express();
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
+//Tells express to format into JSON
 app.use(express.json());
 
-
-
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname),'/public/notes.html')
+)
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
